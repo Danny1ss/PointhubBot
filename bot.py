5387586eff8af9480b1fb5622ead9fea61f35dc0
Dnyr <149859113+Danny1ss@users.noreply.github.com>
2026-05-05 14:07:32 +0200

    Update bot token retrieval and startup message

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from config import BOT_TOKEN
+import os
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=os.getenv("8611419165:AAHtnbEpdOP9o_sjSw4FZ9jfMgUnVxgwwkI"))
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer("🔥 Pointhub Bot is running!")
+    await message.answer("🔥 Bot is running!")
 
 executor.start_polling(dp, skip_updates=True)
